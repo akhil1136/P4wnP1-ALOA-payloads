bc4b8dc1228567e30da5204fed30605d0830754d
akhil1136 <47243201+akhil1136@users.noreply.github.com>
2019-02-18 22:31:26 +0530

    Create lightning speed.js

---
+++ lightning speed.js
@@ -0,0 +1,22 @@
+//lightning speed move powershell or cmd
+function win10AsAdmin() {
+  press("GUI"); //open search
+  delay(200);
+  type("powershell"); //powershell 
+  delay(500); // wait for search to finish
+  press("CTRL SHIFT ENTER"); //start with CTRL+SHIFT+ENTER (run as admin)
+  delay(1000); //wait for confirmation dialog (no check if a password is required, assume login user is admin)
+  press("SHIFT TAB"); //switch to dialog confirmation
+  press("ENTER");
+}
+win10AsAdmin();
+delay(200);
+ press("ALT SPACE m");
+delay(1000); 
+for (var i = 0; i<100; i++){
+press("down")+i
+}
+press("ENTER");
+type("ipconfig");
+press("ENTER");
+delay(1000);
